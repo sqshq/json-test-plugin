5d2e824517236f5b65fa80e7288ea5877b8cdb30
GitHub Action <action@github.com>
2025-05-16 06:06:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/sqshq/jsontestplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/sqshq/jsontestplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.sqshq.jsontestplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
